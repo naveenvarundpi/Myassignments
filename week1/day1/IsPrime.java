efa8763247ef4442b22d6663f95088944a042325
naveenvarundpi <129594404+naveenvarundpi@users.noreply.github.com>
2023-04-01 16:55:51 +0530

    Add files via upload

--- week1/day1/IsPrime.java
+++ week1/day1/IsPrime.java
@@ -3,24 +3,24 @@ package week1.day1;
 public class IsPrime {
 	public static void main(String[] args)
 	{
-		int n=487;
+		int n=173;
 		int prime;
 		int check = 0;
 		for (int i=2;i<=n-1;i=i+1)
 		{
-			System.out.println(i);
+			//System.out.println(i);
 			prime=n%i;
 			//System.out.println(prime);
 			if (prime == 0)
 			{
-				System.out.println("number:"+n+"is not prime");
+				System.out.println("Number: "+n+" is not prime");
 				check=1;
 				break;
 			}	
 		}
 		if(check==0)
 		{
-			System.out.println("number:"+n+"is prime");
+			System.out.println("Number:"+n+" is prime");
 		}
 	}
 }
